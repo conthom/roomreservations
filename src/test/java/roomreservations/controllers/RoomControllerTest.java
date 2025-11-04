f786336bb66beb9f05aac433b95db99932760b11
conthom <conthom@iu.edu>
2025-11-04 14:09:59 -0500

    added hashing passwords, viewing / cancelling reservations, search room tests

--- src/test/java/roomreservations/controllers/RoomControllerTest.java
+++ src/test/java/roomreservations/controllers/RoomControllerTest.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -15,11 +20,48 @@ public class RoomControllerTest {
 	private MockMvc mockMvc;
 
 	@Test
-	void testRooms() throws Exception 	{
+	void testRoomsLoads() throws Exception 	{
 		mockMvc.perform(get("/rooms"))
 				.andExpect(status().isOk())
 				.andExpect(view().name("rooms"))
 				.andExpect(model().attributeExists("rooms"));
+	}
+	@Test
+	void testSearchRooms_BuildingAndCapacity() throws Exception {
+		LocalDate date = LocalDate.now().plusDays(1);
+		LocalTime start = LocalTime.of(10, 0);
+		LocalTime end = LocalTime.of(11, 0);
 
+		mockMvc.perform(get("/rooms/search")
+						.param("building", "Main Building")
+						.param("capacity", "10")
+						.param("date", date.toString())
+						.param("startTime", start.toString())
+						.param("endTime", end.toString()))
+				.andExpect(status().isOk())
+				.andExpect(view().name("reservation_form"))
+				// Should be two rooms in Main Building with at least 10 capacity
+				.andExpect(model().attribute("rooms", hasSize(2)))
+				.andExpect(model().attributeExists("reservation"))
+				.andExpect(model().attributeExists("reservations"));
+	}
+	@Test
+	void testSearchRooms_ByProjectorAndWhiteboard() throws Exception {
+		LocalDate date = LocalDate.now().plusDays(1);
+		LocalTime start = LocalTime.of(9, 0);
+		LocalTime end = LocalTime.of(10, 0);
+
+		mockMvc.perform(get("/rooms/search")
+						.param("hasProjector", "true")
+						.param("hasWhiteboard", "true")
+						.param("date", date.toString())
+						.param("startTime", start.toString())
+						.param("endTime", end.toString()))
+				.andExpect(status().isOk())
+				.andExpect(view().name("reservation_form"))
+				// Should be 5 rooms with both projectors and whiteboards
+				.andExpect(model().attribute("rooms", hasSize(5)))
+				.andExpect(model().attributeExists("reservation"))
+				.andExpect(model().attributeExists("reservations"));
 	}
 }
